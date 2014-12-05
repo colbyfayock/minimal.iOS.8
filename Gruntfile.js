4e578d3a8780778fcd2e5236df19b3f57ca195d2
Colby Fayock <fayock@gmail.com>
2014-12-04 21:38:49 -0500

    fixing gruntfile?

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,25 +4,27 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     debian_package: {
-      options: {
-          maintainer: {
-              name: "Colby Fayock",
-              email: "info@colbyfayock.com"
-          },
-          name: "mininal.iOS.8",
-          short_description: "Minimal winterboard theme for iOS8",
-          long_description: "Minimal winterboard theme for iOS8",
-          version: "1.1.1",
-          dependencies: "winterboard"
-      },
-      files: [
-          {
-              src: [
-                  'minimal.iOS - Icons/**'
-              ],
-              dest: '/Library/Themes/'
-          }
-      ]
+      dist: {
+        options: {
+            maintainer: {
+                name: "Colby Fayock",
+                email: "info@colbyfayock.com"
+            },
+            name: "mininal.iOS.8",
+            short_description: "Minimal winterboard theme for iOS8",
+            long_description: "Minimal winterboard theme for iOS8",
+            version: "1.1.1",
+            dependencies: "winterboard"
+        },
+        files: [
+            {
+                src: [
+                    'minimal.iOS - Icons/**'
+                ],
+                dest: 'Library/Themes/'
+            }
+        ]
+      }
     }
   });
   
