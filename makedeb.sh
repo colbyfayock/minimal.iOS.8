9df69f833b7c267d38aab278a6630b3a07a4ee27
Colby Fayock <fayock@gmail.com>
2014-12-06 13:01:53 -0500

    adding control file. adding deb build script

---
+++ makedeb.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+mkdir Package;
+
+mkdir Package/DEBIAN;
+cp control Package/DEBIAN/control;
+
+mkdir Package/Library;
+mkdir Package/Library/Themes;
+find . -maxdepth 1 -type d -name "minimal.iOS*" -exec cp -r {} Package/Library/Themes/ \;
+
+dpkg-deb -b Package;
+mv Package.deb $1.deb
+
+rm -rf Package;
+
+echo "Oh hey, $1.deb should now be good to go!";
