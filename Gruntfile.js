aeac3453859ffbe7a8d90195b2b079200545d047
Colby Fayock <fayock@gmail.com>
2015-06-23 19:08:29 -0400

    updating grunt settings and workflow

--- Gruntfile.js
+++ Gruntfile.js
@@ -52,10 +52,106 @@ module.exports = function(grunt) {
                 ]
             }
 
+        },
+
+        makedeb: {
+
+            dev: {
+                options: {
+                    name: 'minimal.iOS.8',
+                    control: 'control-dev',
+                    postfix: '.dev'
+                }
+            },
+
+            prod: {
+                options: {
+                    name: 'minimal.iOS.8',
+                    control: 'control-prod'
+                }
+            }
+
+        },
+
+        rsync: {
+
+            options: {
+                args: ["-avhzS --progress"],
+                recursive: true
+            },
+
+            dev: {
+                options: {
+                    src: "dist/mios",
+                    dest: "/Library/Themes",
+                    host: "root@192.168.1.175"
+                }
+            }
+
         }
 
 
     });
+
+    grunt.registerMultiTask('makedeb', 'Build mios .deb using dpkg-deb', function() {
+
+        var data = this.data.options,
+            exec = require('child_process').execSync,
+            pkg_name,
+            pkg_version,
+            controlSplit = exec( "cat " + data.control, { encoding: 'utf8' } ).split('\n'),
+            control = {};
+
+        for ( var i = 0, controlLen = controlSplit.length; i < controlLen; i++ ) {
+            var item = controlSplit[i].split(': ');
+            control[item[0].toLowerCase()] = String(item[1]).trim();
+        }
+
+        pkg_name = data.name && data.name !== '' ? data.name : false;
+        pkg_version = control.version && control.version !== '' ? control.version : false;
+        pkg_file = '';
+
+        if ( pkg_name ) pkg_file += pkg_name.replace(/ /g,"_");
+        if ( data.postfix && data.postfix !== '' ) pkg_file += data.postfix;
+        if ( pkg_version ) pkg_file += '-' + pkg_version;
+
+        // make deb directories
+
+        exec( "mkdir -p Package/DEBIAN" );
+        exec( "mkdir -p Package/Library/Themes" );
+
+        // copy control
+
+        exec( "cp " + data.control + " Package/DEBIAN/control" );
+
+        exec( "find ./dist -maxdepth 1 -type d -name \"" + pkg_name + "*\"", { encoding: 'utf8' } ).split(/\n/).forEach(function(dir) {
+            if ( dir.length > 0 ) {
+                exec( "cp -r " + dir.replace(/ /g,"\\ ") + " Package/Library/Themes/" );
+            }
+        });
+
+        if ( exec( "dpkg-deb --version", { encoding: 'utf8' } ).indexOf('See dpkg-deb --licence for details') === -1 ) {
+            exec( "rm -rf Package" );
+            grunt.log.error(['Oops, you need dpkg-deb to run this task']);
+            grunt.fail.fatal( 'Exiting...');
+
+            return false;
+        }
+
+        try {
+            exec( "dpkg-deb -b Package" );
+        } catch(e) {
+            grunt.fail.fatal( 'Exiting...');
+            return false;
+        }
+
+        exec( "mv Package.deb " + pkg_file  + ".deb" );
+
+        exec( "rm -rf Package" );        
+
+
+    });
+
 
     grunt.registerTask('default', [
         'concat:build'
@@ -69,6 +165,18 @@ module.exports = function(grunt) {
     grunt.registerTask('test', [
         'concat:build',
         'extendscript:test_files'
+    ]);
+
+    grunt.registerTask('sync', [
+        'rsync:dev'
+    ]);
+
+    grunt.registerTask('dev', [
+        'makedeb:dev'
+    ]);
+
+    grunt.registerTask('prod', [
+        'makedeb:prod'
     ]);
 
 };
--- rsyncmios.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-rsync -avhzS --progress dist/mios root@192.168.1.175:/Library/Themes
