bf376acdd17bfd4d00fd6b640430c8d144063ffb
FireBook <git@suat.be>
2015-07-22 04:00:21 +0200

    Fixed bundle id

--- lib/mios_icons.jsx
+++ lib/mios_icons.jsx
@@ -6449,7 +6449,7 @@ mios.icons.push({
 
 mios.icons.push({
     name: 'Vainglory',
-    bundle_id: 'com.superevilmegacorp.vainglory.iphone',
+    bundle_id: 'com.superevilmegacorp.kindred',
     app_id: 'GameKindred.app',
     psd_id: 'vainglory',
     icons: {
